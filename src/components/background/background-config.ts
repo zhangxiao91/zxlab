b2eed6a8cc4d0538d6a25fd4e97339673c4fade8
zhangxiao91 <13372923757@qq.com>
2026-07-12 16:24:21 +0800

    feat(home): unify welcome and motion systems

---
+++ src/components/background/background-config.ts
@@ -0,0 +1,23 @@
+export const BACKGROUND_CONFIG = Object.freeze({
+  patternAngle: 10,
+  farOpacity: 0.095,
+  farDuration: 38,
+  midOpacity: 0.165,
+  midDuration: 21,
+  sparkCount: 14,
+});
+
+export const METEOR_CONFIG = Object.freeze({
+  minLength: 70,
+  maxLength: 260,
+  minDuration: 280,
+  maxDuration: 900,
+  minHeadSize: 1.5,
+  maxHeadSize: 4.5,
+  maxActiveCount: 10,
+  mobileMaxActiveCount: 6,
+  ambientMinInterval: 8000,
+  ambientMaxInterval: 16000,
+  fadeStartViewport: 0.9,
+  fadeEndViewport: 2.2,
+});
